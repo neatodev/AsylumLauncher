e939870d944269ddb9cf450f1ae8e97f9b768382
Neato <49599979+neatodev@users.noreply.github.com>
2024-03-31 00:22:31 +0100

    fix incorrect assignment

--- Data/Display/IniWriter.cs
+++ Data/Display/IniWriter.cs
@@ -156,7 +156,7 @@ namespace AsylumLauncher
             short Framecap = short.Parse(Program.MainWindow.FrameCapTextBox.Text.Trim());
             if (Framecap <= 24)
             {
-                Program.MainWindow.FrameCapTextBox.Text = "62";
+                Program.MainWindow.FrameCapTextBox.Text = "60";
                 IniHandler.BmEngineData["Engine.GameEngine"]["MaxSmoothedFrameRate"] = "62";
             }
             else
